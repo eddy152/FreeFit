ca782006a9e69988ec8a24dc79b2f310ede2360d
hyteoa <hyteotu7@gmail.com>
2021-04-20 16:54:43 +0900

    mem_reg_id ->fitness_id 로 변환

--- TeamFirst/src/main/java/co/team/security/service/MemberVO.java
+++ TeamFirst/src/main/java/co/team/security/service/MemberVO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class MemberVO {
 	private String id;
 	private String password;
-	private Integer mem_reg_id; // admin, ff_user, trainer 구분
+	private Integer fitness_id; // admin, ff_user, trainer 구분
 	private String phone_number;
 	private String name;
 	private String email;
