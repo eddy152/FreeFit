a9b0009338b3f8bc346c0c5d3aea18547e788cf6
hyteoa <hyteotu7@gmail.com>
2021-04-23 18:21:35 +0900

    +

--- TeamFirst/src/main/java/co/team/security/controller/MembershipController.java
+++ TeamFirst/src/main/java/co/team/security/controller/MembershipController.java
@@ -1,5 +1,7 @@
 package co.team.security.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.util.concurrent.Service;
+
+import co.team.admin.service.AdminVO;
 import co.team.security.service.MemberService;
 import co.team.security.service.impl.MemberMapper;
 
@@ -33,18 +38,23 @@ public class MembershipController {
 	//멤버십 등록 페이지 (grade 로 등급넘김)
 	@GetMapping("/msRegister")
 	public String msRegister(String grade,Model model, HttpSession session) {
-		String id=(String)session.getAttribute("id");
-		
 		//로그인여부는 security 로 거르고
+		String id=(String)session.getAttribute("id");
+		AdminVO member=new AdminVO();
+		member.setAdmin_id(id);
+		List<AdminVO> membership=memberService.ownerMemberships(member);
+		if (membership ==null) {
+			
+		}
 		//모델에 등급값, 아이디, 현재 멤버십 등록현황 받아서
 		model.addAttribute("id",id);
 		//멤버십 있으면 추가페이지로
 		
-		
 		return "homepage/membership/msRegister";
 	}
 	
-	//멤버십 
+	//멤버십 결제 페이지
+	
 	
 	
 }
